35708f043abf9c8b2a7f9f8752426fc2bd1eb500
Vivek Gurumoorthy <108832056+vivek-gurumoorthy@users.noreply.github.com>
2022-08-30 21:01:18 -0400

    Update queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -43,3 +43,20 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+-- Create emp_info table with emp_no, first and last name, gender, and salary
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+     AND (de.to_date = '9999-01-01');
