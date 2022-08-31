0b43e849641c9a8d2dc12821f8ca44ca3679113d
Vivek Gurumoorthy <108832056+vivek-gurumoorthy@users.noreply.github.com>
2022-08-30 20:52:18 -0400

    Update queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -35,3 +35,11 @@ LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+INTO count_by_dept
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
