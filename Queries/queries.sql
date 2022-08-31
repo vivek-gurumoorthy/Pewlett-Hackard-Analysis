aa864fdabccf9f9ccef802e0ea4bd2c8889e2613
Vivek Gurumoorthy <108832056+vivek-gurumoorthy@users.noreply.github.com>
2022-08-30 20:39:47 -0400

    Update queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -16,10 +16,22 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 SELECT * FROM retirement_info;
 
 -- Joining departments and dept_manager tables
-SELECT departments.dept_name,
-     dept_manager.emp_no,
-     dept_manager.from_date,
-     dept_manager.to_date
-FROM departments
-INNER JOIN dept_manager
-ON departments.dept_no = dept_manager.dept_no;
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables into current_emp table
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
