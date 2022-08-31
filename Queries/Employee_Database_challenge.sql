27a891f268cacfb526a84be9027f1b8c43ea949a
Vivek Gurumoorthy <108832056+vivek-gurumoorthy@users.noreply.github.com>
2022-08-31 11:49:28 -0400

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -31,3 +31,21 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+--INTO mentorship_eligibility
+FROM employees as e
+  INNER JOIN dept_emp as de
+    ON (e.emp_no = de.emp_no)
+  INNER JOIN title as t
+    ON (e.emp_no = t.emp_no) 
+WHERE (de.to_date = '9999-01-01') AND
+  (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
